89cc241f22edc8901d0f42e94f72bf0acbf80b77
JamshidPardayev <jamshidnt2024@gmail.com>
2025-05-14 11:46:37 +0500

    contact qushildi

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div id='contact'>
-      <h1>Contact</h1>
+    <div id='contact' className='max-w-[1200px] min-h-[377px] mx-auto  bg-[#F6F6F6] mt-[100px] mb-[30px] flex  justify-between items-center gap-10 px-[100px] rounded-[22px] py-[20px] max-md:flex-col max-md:text-center max-lg:px-[60px] max-md:px-[20px]'>
+      <div>
+        <h1 className='text-[36px] text-[#1E1D1D] font-bold '>Subscribe To Newsletter</h1>
+        <p className='text-[16px] text-[#8B8E99] font-medium my-[12px]'>Get free guide about smart watches daily. </p>
+        <form action="" className='flex items-center max-w-[443px] bg-white h-[60px] rounded-[15px]'>
+          <input type="email" required placeholder='Enter Email Address' className='bg-white w-full h-full px-3 outline-none rounded-[15px] border-none'/>
+          <button className='w-[131px] h-[45px] text-white bg-[#3858D6] hover:bg-blue-800 duration-300 rounded-[9px] cursor-pointer mr-[9px]'>Subscribe</button>
+         </form>
+      </div>
+      <div>
+        <img src="./image/contactImg.png" alt="contactimage" className=''/>
+      </div>
     </div>
   )
 }
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -6,8 +6,8 @@ const Hero = () => {
         <div>
          <h1 className='text-white text-[64px] max-w-[420px] font-bold leading-[125%] tracking-[0%] '>Discover Most Suitable Watches</h1>
          <p className='text-[16px] text-[#8B8E99] max-w-[410px] tracking-[0%] font-medium mt-[20px]'>Find the best, reliable, and cheap smart watches here. We focus on product quality. Here you can find smart watches of almost all brands. So why you are waiting? Just order now!</p>
-         <form action="" className='flex items-center max-w-[443px] border bg-white mt-[40px]  h-[60px] rounded-[15px]'>
-          <input type="text" placeholder='Find the best brands' className='bg-white w-full h-full px-3 outline-none rounded-[15px]'/>
+         <form action="" className='flex items-center max-w-[443px] bg-white mt-[40px]  h-[60px] rounded-[15px]'>
+          <input type="text" placeholder='Find the best brands' className='bg-white w-full h-full px-3 outline-none rounded-[15px] border-none'/>
           <button className='w-[112px] h-[45px] text-white bg-[#3858D6] hover:bg-blue-800 duration-300 rounded-[9px] cursor-pointer mr-[9px]'>Search</button>
          </form>
         </div>
